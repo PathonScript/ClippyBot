d8b678ee8d66f73fd45d4537631203c31c075cc4
PathonScript <pathonscript@gmail.com>
2022-08-04 19:08:14 +0700

    Basic Setup
    Ping Command
    
    Command Handler from Conaticus

---
+++ src/deploy-commands.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { commandFiles } from "./files";
+import { BotCommand } from "./types";
+dotenv.config();
+
+const commands: object[] = [];
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file.substring(
+        0,
+        file.length - 3
+    )}`) as BotCommand;
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(process.env.TOKEN as string);
+
+rest.put(
+    Routes.applicationCommands(
+        process.env.CLIENT_ID as string,
+    ),
+    { body: commands }
+);
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import "./deploy-commands";
+
+import { Client, Collection, IntentsBitField } from "discord.js";
+import { BotClient, BotCommand } from "./types";
+import { commandFiles, eventFiles } from "./files";
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.GuildPresences,
+    	IntentsBitField.Flags.GuildMembers
+	],
+}) as BotClient;
+client.commands = new Collection();
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`) as BotCommand;
+    client.commands.set(command.data.name, command);
+}
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file.substring(0, file.length - 3)}`);
+
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+        continue;
+    }
+
+    client.on(event.name, (...args) => event.execute(...args, client));
+}
+
+client.login(process.env.TOKEN);
