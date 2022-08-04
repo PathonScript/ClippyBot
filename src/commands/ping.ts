084aebf9388b67d90a6b1023b6562dd7b102d24e
PathonScript <pathonscript@gmail.com>
2022-08-04 19:14:52 +0700

    eslint and ping ms

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,6 +4,16 @@ module.exports = {
         .setName("ping")
         .setDescription("Replies with a pong!"),
     async execute(interaction: any) {
-        interaction.reply("Pong!");
+        const time = new Date();
+        await interaction.deferReply();
+        await interaction.editReply('Pong !');
+
+        try {
+            const ms = (time.getMilliseconds() - new Date().getMilliseconds());
+            await interaction.editReply(`Pong !\n${ms} ms`);
+        } catch (error: any) {
+            await interaction.editReply('PingCommand is unavailable.');
+            throw new Error(error.message);
+        }
     },
 };
