f00f522b61c567f0466a60434f4fb89cda3c196b
PathonScript <pathonscript@gmail.com>
2022-08-04 21:49:30 +0700

    clip command args

--- src/commands/clip.ts
+++ src/commands/clip.ts
@@ -1,16 +1,36 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
 module.exports = {
+	
 	data: new SlashCommandBuilder()
 		.setName('clip')
-		.setDescription('Create a Clip!'),
+		.setDescription('Create a Clip!')
+		.addStringOption(option => 
+			option.setName('category')
+				.setDescription('The category of the clip.')
+				.setRequired(true)
+		)
+		.addStringOption(option =>
+			option.setName('content')
+				.setDescription('The content of the clip.')
+				.setRequired(true)
+		)
+	
+	,
+	
 	async execute(interaction: any) {
+		const category = interaction.options.getString("category")
+		const content = interaction.options.getString("content")
+
         const time = new Date();
         await interaction.deferReply();
-        await interaction.editReply('Clip !');
+        await interaction.editReply(`🔃Creating clip of ${content} in the ${category} category...`);
 
         try {
             const ms = (time.getMilliseconds() - new Date().getMilliseconds());
-            await interaction.editReply(`Clip command working !\n${ms} ms`);
+            await interaction.editReply(`✅Created clip of ${content} in the ${category} successfully`);
         } catch (error: any) {
             await interaction.editReply('PingCommand is unavailable.');
             throw new Error(error.message);
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,10 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 module.exports = {
+    
     data: new SlashCommandBuilder()
         .setName("ping")
         .setDescription("Replies with a pong!"),
+    
     async execute(interaction: any) {
         const time = new Date();
         await interaction.deferReply();
